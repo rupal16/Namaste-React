f811bfcc4f543ebe300375c1f5b288730d9c6661
Rupal Jain <rupaljain.1699@gmail.com>
2023-12-29 19:52:08 +0530

    react.createElement concept

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
+{/* 
+<div id="parent" >
+    <div id="child" >
+        <h1></h1>
+    </div>
+</div> 
+*/}
+
+
+const parent = React.createElement("div", { id: "parent" },
+    React.createElement("div", { id: "child" },
+        [
+            React.createElement("h1", {}, "Hello World"),
+            React.createElement("h1", {}, "Hello World")
+        ]
+    )
+)
+
+console.log(parent )
+
 const heading = React.createElement("h1", { id: "heading" }, "Hello World from React!");
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(parent ); 
