795905d401b9dd695f12790102a32f5c8001e911
Rupal Jain <rupaljain.1699@gmail.com>
2024-01-10 18:35:48 +0530

    added custom hook - useOnline status

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,20 @@
+import useOnlineStatus from "../utils/useOnlineStatus";
+
 const About = () => {
+
+    const onlineStatus = useOnlineStatus();
+    console.log(onlineStatus, "hi")
+
+    console.log("bye")
+
+    if(!onlineStatus){
+        return <h1>You are offline</h1>;
+    }
+
     return (
         <div>
-            <div>Hii</div>
-            <div>This is about page.</div>
+            {/* <div>Hii</div> */}
+            <div>This is about hi page.</div>
         </div>
     )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 const Header = () => {
-    return <div>Header</div>
+    return <div>Header hi</div>
 }
 
 export default Header;
